3a3470724b8bcfb63fcfdb9b70bcaf75b027619d
owlPickle <terry998877terry@gmail.com>
2019-08-19 13:34:28 +0800

    draw restarunt function

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
   let list = document.getElementById("eating-list");
   let selectInput = document.getElementById("area");
   let story = document.getElementById("story");
+  let randomBtn = document.getElementById("random");
   let html = '';
   // Load Data
   const restarunt = [];
   const area = [];
+  const selectArr = [];
+  const data = await axios.get("./data.json");
 
-  let getData = async function () {
-    const data = await axios.get("./data.json");
-
-    restarunt.push(...data.data);
-    // render restarunt list
-    restarunt.forEach((store, idx) => {
-      area.push(store.district);
-      html = `
+  restarunt.push(...data.data);
+  // render restarunt list
+  restarunt.forEach((store, idx) => {
+    area.push(store.district);
+    html = `
       <div class="restarunt">
         <div class="title">${store.name}</div>
         <div data-region="${store.district}" class="info">
@@ -24,53 +24,59 @@
         </div>
       </div>
     `
-      list.innerHTML += html;
-    });
+    list.innerHTML += html;
+  });
 
-    const uniqDistrict = area.reduce(function (prev, now) {
-      if (prev.indexOf(now) < 0) prev.push(now);
-      return prev;
-    }, []);
+  const uniqDistrict = area.reduce(function (prev, now) {
+    if (prev.indexOf(now) < 0) prev.push(now);
+    return prev;
+  }, []);
 
-    // click render info
-    let eatList = document.querySelectorAll("#eating-list .title");
-    let showInfo = function () {
-      this.nextElementSibling.classList.toggle("open");
-      let storyHtml = '';
-      restarunt.forEach(res => {
-        if (res.name === this.innerText) {
-          storyHtml = `
-            <h1>餐廳介紹</h1>
+  // click render info
+  let eatList = document.querySelectorAll("#eating-list .title");
+  let showInfo = function () {
+    this.nextElementSibling.classList.toggle("open");
+    let storyHtml = '';
+    restarunt.forEach(res => {
+      if (res.name === this.innerText) {
+        storyHtml = `
+            <h1>${res.name}</h1>
             <p>${res.introduction}</p>
           `
-          story.innerHTML = storyHtml;
-        }
-      })
-    }
-    let selectArea = function () {
-      eatList.forEach(region => {
-        region.parentElement.style.display = "none";
-        if (region.nextElementSibling.getAttribute("data-region") === this.value) {
-          region.parentElement.style.display = "block";
-        }
-      })
-    }
-
-    uniqDistrict.forEach(region => {
-      let option = document.createElement('option');
-      option.value = region;
-      option.textContent = region;
-      selectInput.appendChild(option);
+        story.innerHTML = storyHtml;
+      }
     })
-    //選擇地區，render該地區的餐廳
-    selectInput.addEventListener("change", selectArea)
-
-    eatList.forEach(title => {
-      title.addEventListener("click", showInfo);
-    })
-
   }
-  getData();
+  let selectArea = function () {
+    eatList.forEach(region => {
+      region.parentElement.style.display = "none";
+      if (region.nextElementSibling.getAttribute("data-region") === this.value) {
+        selectArr.push(region);
+        region.parentElement.style.display = "block";
+        randomBtn.disabled = false;
+      }
+    })
+  }
 
+  let draw = function (e) {
+    e.preventDefault();
+    
+    let randomNum = Math.floor(Math.random() * Math.floor(selectArr.length));
+    selectArr[randomNum].click();
+  }
+
+  uniqDistrict.forEach(region => {
+    let option = document.createElement('option');
+    option.value = region;
+    option.textContent = region;
+    selectInput.appendChild(option);
+  })
+  //選擇地區，render該地區的餐廳
+  selectInput.addEventListener("change", selectArea);
+  randomBtn.addEventListener("click", draw);
+
+  eatList.forEach(title => {
+    title.addEventListener("click", showInfo);
+  })
 
 })();
